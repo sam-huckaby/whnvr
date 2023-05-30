5659cea67f4204294de19cd19ae76fa20b453413
Sam Huckaby <samhuckaby@gmail.com>
2023-05-29 23:14:44 -0400

    Okay, time to admit defeat and try rapper

--- lib/database/database.ml
+++ lib/database/database.ml
@@ -2,12 +2,38 @@
 module type DB = Caqti_lwt.CONNECTION
 module T = Caqti_type
 
+type db_post = {
+  id : int ;
+  message : string ;
+  username : string ;
+  display_name : string ;
+  created : string ;
+}
+
 (* This is a very SIMPLY database query. Needs to be wrapped in something, so it can be standardized *)
 let list_posts =
   let query =
     let open Caqti_request.Infix in
     (T.unit ->* T.(tup4 string string string string))
-    "SELECT posts.message, users.username, users.display_name, to_char(posts.created, 'MM-DD-YYYY @ HH:MI') AS created FROM posts JOIN users ON posts.user_id = users.id" in
+    "SELECT posts.message,
+            users.username,
+            users.display_name,
+            to_char(posts.created, 'MM-DD-YYYY @ HH:MI') AS created
+    FROM posts
+    JOIN users ON posts.user_id = users.id" in
   fun (module Db : DB) ->
     let%lwt posts_or_error = Db.collect_list query () in
     Caqti_lwt.or_fail posts_or_error
+
+
+let query_posts =
+  [%rapper
+    get_opt
+      {sql|
+        SELECT @int{posts.id},
+               @string{posts.message},
+               @string{users.username},
+               @string{users.display_name},
+               @string{to_char(posts.created, 'MM-DD-YYYY @ HH:MI') AS created}
+      |sql}
+  ]
